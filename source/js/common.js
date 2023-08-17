afbd67b310acac539edd90845d50fea611a4745e
Murad <abishlimurad@gmail.com>
2023-08-17 21:05:36 +0300

    final landing

--- source/js/common.js
+++ source/js/common.js
@@ -1,18 +1,40 @@
+const tabButtons = document.querySelectorAll('.tab-toggle'),
+      tabContents = document.querySelectorAll('.tab-content')
+
+tabContents[0].classList.add('tab-content--active');
+tabButtons[0].classList.add('tab-toggle--active');
+
+tabButtons.forEach(tabButton => {
+  tabButton.addEventListener('click', () => {
+    const tabId = tabButton.getAttribute('data-tab');
+
+    tabContents.forEach(content => {
+      if (content.id === tabId) {
+        content.classList.add('tab-content--active');
+      } else {
+        content.classList.remove('tab-content--active');
+      }
+    });
+
+    tabButtons.forEach(button => {
+      if (button === tabButton) {
+        button.classList.add('tab-toggle--active');
+      } else {
+        button.classList.remove('tab-toggle--active');
+      }
+    });
+  });
+});
+
+
 if (Swiper) {
-  const quotersSlider = new Swiper('.quotes__hidden', {
-		effect: 'coverflow',
-		slidesPerView: 1,
+  const quotersSlider = new Swiper('.lessons-swiper', {
+		slidesPerView: 'auto',
     spaceBetween: 20,
     speed: 500,
-    pagination: {
-      el: '.quotes__slider-pag', // Элемент контейнера буллетов
-      clickable: true, // Сделать буллеты кликабельными
-    },
-  })
-}
-
-if (AOS) {
-  AOS.init({
-    once: true
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    }
   })
 }
